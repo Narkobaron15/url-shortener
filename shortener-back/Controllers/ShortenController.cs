197431c587197e7235aeec0bf15018038566590c
Alex Ivanov <oleksii120@gmail.com>
2024-06-20 18:17:28 +0300

    Started adding shortening gateways & logic

---
+++ shortener-back/Controllers/ShortenController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shortener_back.Controllers;
+
+[ApiController, Route("")/*, Authorize*/]
+public class ShortenController(
+    IConfiguration configuration
+) : ControllerBase
+{
+    [HttpPut]
+    public async Task<IActionResult> Shorten([FromBody] string url)
+    {
+        // call service and add to database
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        string mainPage = configuration["MainPage"]
+                          ?? String.Empty;
+        return RedirectPermanent(mainPage);
+    }
+
+    [HttpGet("{code}")]
+    public async Task<IActionResult> Get(string code)
+    {
+        // call service and redirect
+        // string url = await urlService.Get(code);
+        string url = "https://www.google.com";
+        return RedirectPermanent(url);
+    }
+}
