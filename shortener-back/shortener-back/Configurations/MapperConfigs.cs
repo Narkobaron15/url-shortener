f3baaa39a39edfb410ddbd0b6ef82eea9ff2542e
Alex Ivanov <oleksii120@gmail.com>
2024-06-20 23:48:51 +0300

    Added mapper configurations

---
+++ shortener-back/shortener-back/Configurations/MapperConfigs.cs
@@ -0,0 +1,10 @@
+﻿namespace shortener_back.Configurations;
+
+public class MapperConfigs : /*AutoMapper*/ Profile
+{
+    public MapperConfigs()
+    {
+        CreateMap<Shorten, ShortenDto>();
+        CreateMap<User, UserDto>();
+    }
+}
