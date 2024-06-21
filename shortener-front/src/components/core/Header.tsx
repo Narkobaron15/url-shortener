26745d467788e11226d5b031a0fe6fccf56c6b7a
Alex Ivanov <oleksii120@gmail.com>
2024-06-21 15:09:13 +0300

    Added header

---
+++ shortener-front/src/components/core/Header.tsx
@@ -0,0 +1,34 @@
+import {Navbar} from "flowbite-react";
+import './css/Header.css'
+
+export default function Header() {
+    return (
+        <Navbar fluid={true} rounded={true}>
+            <Navbar.Brand href="/">
+                <img
+                    src="https://flowbite.com/docs/images/logo.svg"
+                    className="mr-3 h-6 sm:h-9"
+                    alt="Flowbite Logo"
+                />
+                <h1 className="styled">
+                  ShorterUrl
+                </h1>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+            <Navbar.Collapse>
+                <Navbar.Link href="/" active={true}>
+                    Home
+                </Navbar.Link>
+                <Navbar.Link href="/about">
+                    About
+                </Navbar.Link>
+                <Navbar.Link href="/services">
+                    Services
+                </Navbar.Link>
+                <Navbar.Link href="/account">
+                    Account
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
