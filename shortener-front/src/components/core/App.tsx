358035786fb9cf096ce6984317b0ca6f52b71922
Alex Ivanov <oleksii120@gmail.com>
2024-06-21 15:09:02 +0300

    Added flowbite

--- shortener-front/src/components/core/App.tsx
+++ shortener-front/src/components/core/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import './css/App.css'
 import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout.tsx";
+import HomePage from "../pages/HomePage.tsx";
+import NotFound from "../pages/NotFound.tsx";
 
 function App() {
     // TODO: add router
     return (
         <Routes>
-            <Route path="/">
+            <Route path="/" element={<Layout/>}>
                 <Route index element={<HomePage/>}/>
             </Route>
             <Route path="*" element={<NotFound/>}/>
--- shortener-front/src/main.tsx
+++ shortener-front/src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './components/core/App.tsx'
 import './index.css'
+import {BrowserRouter} from "react-router-dom";
+import 'flowbite'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
